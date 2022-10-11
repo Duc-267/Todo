0df1a0f90b4b0a5a8a5c8859cb06fdde15e97196
Nghia <nghia.phan.gg@gmail.com>
2022-10-11 20:53:44 +0700

    find projectID

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,11 @@
 const projectList = document.querySelector(".project-container__list");
 let projects = JSON.parse(localStorage.getItem("project")) || [];
 let idCounter = Math.floor(Math.random() * 100000);
+const currentUser = JSON.parse(localStorage.getItem(KEY_CURRENT_USER));
+document.getElementById("name").innerHTML = currentUser.fullName
+document.getElementById("id").innerHTML = currentUser.id
+
+ console.log(currentUser.email)
 
 
 const renderProject = function () {
@@ -30,3 +35,58 @@ const addProject = function(){
   renderProject()
 }
 renderProject();
+
+  var user_project = [
+    {
+        email: 'nghia@gmail.com',
+
+        projectId: 1,
+        
+        isAdmin: true ,
+    },
+    {
+        email: 'admin@gmail.com',
+
+        projectId: 3,
+        
+        isAdmin: true ,
+    },
+    {
+        email: "nghia@gmail.com",
+
+        projectId: 2,
+        
+        isAdmin: false ,
+    },
+    {
+        email: '1@gmail.com',
+
+        projectId: 1,
+        
+        isAdmin: true ,
+    },
+    {
+        email: '1@gmail.com',
+
+        projectId: 2,
+        
+        isAdmin: false ,
+    },
+    {
+        email: "nghia@gmail.com",
+
+        projectId: 3,
+        
+        isAdmin: false ,
+    }
+]
+localStorage.setItem('user-project', JSON.stringify(user_project));
+const userProject = JSON.parse(localStorage.getItem("user-project"));
+
+// if (userProject.filter(item => item.email == JSON.stringify(currentUser.email))) {
+//   // console.log(userProject.filter(item => item.projectId))
+// }
+console.log(userProject.filter(item => item.email === currentUser.email))
+console.log("Your ProjectId is" + ' ' + userProject.filter(item => item.email === currentUser.email).map(item => item.projectId).join(' and '))
+
+
